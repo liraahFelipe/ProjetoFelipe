5be09eb0f1b3f36a4a3602e9d2004468545dc5e5
liraahFelipe <134751265+liraahFelipe@users.noreply.github.com>
2023-05-27 18:43:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,10 +15,3 @@ function toggleMode() {
 
 
 }
-
-
- // if(html.classList.contains('light')) {
-   //     html.classList.remove('light')
-   // } else {
-   //    html.classList.add('light')
-   // }
